1b59577bd564d75bc29f87019c44eef0e47ccee4
zendrias <zenas@live.com>
2022-05-13 18:25:23 -0400

    create get and set methods

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 class HashTable {
-  constructor(size = 53) {
+  constructor(size = 5) {
     this.keyMap = new Array(size);
   }
 
@@ -13,4 +13,37 @@ class HashTable {
     }
     return total;
   }
+
+  set(key, value) {
+    const index = this._hash(key)
+    this.keyMap[index] ? (
+      this.keyMap[index].push([key, value])
+    ) : (
+      this.keyMap[index] = [[key, value]]
+    )
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    if (this.keyMap[index]) {
+      const myData = this.keyMap[index].filter(data => {
+        return data[0] === key
+      })
+      return myData[0][1]
+    }
+    return undefined
+  }
+
 }
+
+const ht = new HashTable()
+
+ht.set('secret', 'Zena likes icecream')
+ht.set('dog', 'food')
+ht.set('remote', 'controller')
+ht.set('marco', 'polo')
+ht.set('tommy', 'hilfiger')
+
+
+console.log(ht.get('dog'))
+// console.log(ht)
