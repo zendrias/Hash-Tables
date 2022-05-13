59e51b096909ec9b841dbb7a289a4eb3398333db
zendrias <zenas@live.com>
2022-05-12 22:17:30 -0400

    stub out basic hashing function and layout

---
+++ script.js
@@ -0,0 +1,16 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * prime + value) % this.keyMap.length;
+    }
+    return total;
+  }
+}
