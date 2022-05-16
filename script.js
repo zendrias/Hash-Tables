aa660cc978c50f88a7b5baf3f9313db346a1ff08
zendrias <zenas@live.com>
2022-05-15 21:26:50 -0400

    hash table try 3

--- script.js
+++ script.js
@@ -100,6 +100,71 @@ class HashTable2 {
 }
 
 
+class HashTable {
+  constructor(size = 59) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let prime = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * prime + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    const index = this._hash(key)
+    this.keyMap[index] ? (
+      this.keyMap[index].push([key, value])
+    ) : (
+      this.keyMap[index] = [[key, value]]
+    )
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    if (this.keyMap[index]) {
+      const myData = this.keyMap[index].filter(data => {
+        return data[0] === key
+      })
+      return myData[0][1]
+    }
+    return undefined
+  }
+
+  values() {
+    let values = []
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!values.includes(this.keyMap[i][j][1])) {
+            values.push(this.keyMap[i][j][1])
+          }
+        }
+      }
+    }
+    return values
+  }
+  keys() {
+    let keys = []
+    for (let i = 0; i < this.keyMap.length; i++) {
+      if (this.keyMap[i]) {
+        for (let j = 0; j < this.keyMap[i].length; j++) {
+          if (!keys.includes(this.keyMap[i][j][1])) {
+            keys.push(this.keyMap[i][j][1])
+          }
+        }
+      }
+    }
+    return keys
+  }
+}
+
+
 const ht2 = new HashTable2(5)
 
 ht2.set('kevin', 'guzman')
